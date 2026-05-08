c9f444c2df13d3c0819ac271fde195223c43ddb3
aws-dev-wgl <awsdevlabwgl@gmail.com>
2026-05-07 23:18:35 -0500

    fix: replace var.project with hardcoded tra3 in SSM paths, remove Stripe tfvars

--- backend-integration/terraform/ssm.tf
+++ backend-integration/terraform/ssm.tf
@@ -1,11 +1,13 @@
-data "aws_ssm_parameter" "stripe_secret_key" {
-  name            = var.stripe_secret_key_parameter_name
-  with_decryption = true
+data "aws_ssm_parameter" "square_access_token" {
+  name = "/tra3/${var.client_name}/${var.environment}/square_access_token"
 }
 
-data "aws_ssm_parameter" "stripe_webhook_secret" {
-  name            = var.stripe_webhook_secret_parameter_name
-  with_decryption = true
+data "aws_ssm_parameter" "square_location_id" {
+  name = "/tra3/${var.client_name}/${var.environment}/square_location_id"
+}
+
+data "aws_ssm_parameter" "square_webhook_signature_key" {
+  name = "/tra3/${var.client_name}/${var.environment}/square_webhook_signature_key"
 }
 
 data "aws_ssm_parameter" "textbelt_api_key" {
